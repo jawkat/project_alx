fbf604c872bc4de9e8963351391dcfba54e8f4c0
jawkat <kad.joliano@gmail.com>
2024-07-06 14:10:31 +0100

    add virtual environnment

--- TaskManager/__init__.py
+++ TaskManager/__init__.py
@@ -37,9 +37,11 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_DEFAULT_SENDER'] = 'noreply@demo.com'
 
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
+print(os.environ.get('MAIL_USERNAME'))
+print(os.environ.get('MAIL_PASSWORD'))
 
 mail = Mail(app)
 
